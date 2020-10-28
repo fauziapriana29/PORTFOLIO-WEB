7a53a2107cc0e260fc5cde433dec3f4984e4219b
fauziapriana29 <fauzicoding@gmail.com>
2020-10-28 19:43:23 +0700

    try

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -3,16 +3,19 @@ import React from 'react'
 function FormField() {
     return (
         <div>
-            <form name="contact" method="POST" data-netlify="true">
+            <form name="contact" action="POST" data-netlify="true">
                 <p>
-                    <label>Your Name: <input type="text" name="name" /></label>   
+                    <label>Your Name: <input type="text" name="name" id="name" /></label>   
                 </p>
                 <p>
-                    <label>Your Email: <input type="email" name="email" /></label>
+                    <label>Your Email: <input type="email" name="email" id="email" /></label>
                 </p>
                 <p>
-                    <label>Message: <textarea name="message"></textarea></label>
+                    <label>Message: <textarea name="message" id="message"></textarea></label>
                 </p>
+                <div className="field">
+                    <div data-netlify-recaptcha="true"></div>
+                </div>
                 <p>
                     <button type="submit">Send</button>
                 </p>
